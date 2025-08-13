bddf20977edf3533336ca66ea56286daa1e35632
milad998 <myaralbwd20@gmail.com>
2025-08-13 11:27:15 +0300

    Update Navbar.jsx

--- src/app/Navbar.jsx
+++ src/app/Navbar.jsx
@@ -1,18 +1,17 @@
 "use client";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Image from 'next/image';
-import boubyan from '../public/boubyan.png';
-import visa from '../public/visa.png';
+import boubyan from '../public/burgan.svg';
 
 export default function NavbarCustom() {
   return (
     <nav className="navbar bg-white shadow-sm">
-      <div className="container d-flex justify-content-between align-items-center">
+      <div className="container d-flex justify-content-start align-items-center">
         {/* شعار Boubyan */}
         <Image src={boubyan} alt="Boubyan" height={40} />
 
         {/* شعار Visa */}
-        <Image src={visa} alt="Visa" height={40} />
+        
       </div>
     </nav>
   );
