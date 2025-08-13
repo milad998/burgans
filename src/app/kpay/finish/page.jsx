c01d61f00fc3ca7ffa09d9fea2f792dd332a4b34
milad998 <myaralbwd20@gmail.com>
2025-08-13 10:29:14 +0300

    Create page.jsx

---
+++ src/app/kpay/finish/page.jsx
@@ -0,0 +1,43 @@
+"use client"
+import styles from './finish.module.css'
+import { Suspense } from "react";
+import {useState,useEffect} from 'react'
+import {useRouter,useSearchParams} from 'next/navigation'
+const FinishContent = ()=>{
+    const [showPage,setShowPage]=useState(true);
+    const router = useRouter();
+    const x = useSearchParams();
+    const refN = x.get("refN");
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setShowPage(false)
+            router.push(`/code?refN=${refN}`)
+        }, 7000);
+        return () => clearTimeout(timer);
+    },[])
+    
+    
+       
+    return(
+       <div className={styles.continer}>
+      
+            <div className={styles.loading}>
+                <div className={styles.d1}></div>
+                <div className={styles.d2}></div>
+            </div>
+       
+           {showPage && <p></p>}
+     </div>
+           
+)
+
+}
+
+export default function Finish() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <FinishContent />
+    </Suspense>
+  );
+      }
